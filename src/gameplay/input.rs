8b171fffefdf47f58c5e2295fa395222d788e7ae
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-03-25 12:20:07 -0400

    add vertical padding to paddles

--- src/gameplay/input.rs
+++ src/gameplay/input.rs
@@ -21,36 +21,39 @@ fn paddle_input(
 ) {
     query
         .iter_mut()
-        .for_each(|(mut postition, paddle, height)| match paddle {
-            PaddleType::Ai => {
-                let ball = get_single!(ball);
-                let a_to_b = ball.0 - postition.0;
-                let new_pos = postition.0.y + a_to_b.y.signum() * 1.4;
-                postition.0.y =
-                    new_pos.clamp(0. + height.h / 2., GAME_H - height.h / 2.)
-            }
-            PaddleType::Player => {
-                let up: f32 = if kb_input.any_pressed([
-                    KeyCode::ArrowUp,
-                    KeyCode::KeyK,
-                    KeyCode::KeyW,
-                ]) {
-                    1.
-                } else {
-                    0.
-                };
-                let down: f32 = if kb_input.any_pressed([
-                    KeyCode::ArrowDown,
-                    KeyCode::KeyJ,
-                    KeyCode::KeyS,
-                ]) {
-                    1.
-                } else {
-                    0.
-                };
-                let new_pos = postition.0.y + (up - down) * 2.;
-                postition.0.y =
-                    new_pos.clamp(0. + height.h / 2., GAME_H - height.h / 2.)
+        .for_each(|(mut postition, paddle, height)| {
+            let padding: f32 = 5.;
+            let min: f32 = padding + height.h / 2.;
+            let max: f32 = GAME_H - padding - (height.h / 2.);
+            match paddle {
+                PaddleType::Ai => {
+                    let ball = get_single!(ball);
+                    let a_to_b = ball.0 - postition.0;
+                    let new_pos = postition.0.y + a_to_b.y.signum() * 1.4;
+                    postition.0.y = new_pos.clamp(min, max)
+                }
+                PaddleType::Player => {
+                    let up: f32 = if kb_input.any_pressed([
+                        KeyCode::ArrowUp,
+                        KeyCode::KeyK,
+                        KeyCode::KeyW,
+                    ]) {
+                        1.
+                    } else {
+                        0.
+                    };
+                    let down: f32 = if kb_input.any_pressed([
+                        KeyCode::ArrowDown,
+                        KeyCode::KeyJ,
+                        KeyCode::KeyS,
+                    ]) {
+                        1.
+                    } else {
+                        0.
+                    };
+                    let new_pos = postition.0.y + (up - down) * 2.;
+                    postition.0.y = new_pos.clamp(min, max)
+                }
             }
         });
 }
