0dc8e1aec5cbbc396d1fb66f9e9d30aafbdad422
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-03-25 17:25:18 -0400

    add pause screen interactions

--- src/screens/pause.rs
+++ src/screens/pause.rs
@@ -9,10 +9,21 @@ fn spawn_pause_screen(mut commands: Commands) {
         .ui_root_col()
         .with_children(|children| {
             children.header("PAUSED");
+            children.button("Resume").observe(resume);
+            children.button("Quit").observe(quit);
         })
         .insert((
             StateScoped(Gameplay::Paused),
             BackgroundColor(Color::srgba(0., 0., 0., 0.75)),
             ZIndex(2),
         ));
+}
+
+// BUTTON EVENTS
+fn resume(_trigger: Trigger<OnClick>, mut next: ResMut<NextState<Gameplay>>) {
+    next.set(Gameplay::InPlay);
+}
+
+fn quit(_trigger: Trigger<OnClick>, mut app_exit: EventWriter<AppExit>) {
+    app_exit.send(AppExit::Success);
 }
